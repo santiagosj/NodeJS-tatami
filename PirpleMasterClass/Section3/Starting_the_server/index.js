28fa633c8349f1231df4e7493c5efbe2e922278a
Santi <sanjs965@gmail.com>
2020-09-27 12:47:49 -0300

    parsing request paramsURL

--- PirpleMasterClass/Section3/Starting_the_server/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-const server = http.createServer((req, res)=>{
-    res.end('Hola NodeJS!')
-})
-
-server.listen(3000,function(){
-    console.log('servidor escuchando en el port 3000')
-})
