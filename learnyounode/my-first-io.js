b574ab348df6c5d095a30a2e59dc6d5af56127a7
santiagosj <sanjs965@gmail.com>
2022-09-28 00:05:41 -0300

    learnyounode 3 mejora

--- learnyounode/my-first-io.js
+++ learnyounode/my-first-io.js
@@ -1,6 +1,5 @@
 const fs = require('fs')
 
-const content = fs.readFileSync(process.argv[2]).toString();
-const lines = content.split('\n').length - 1;
+const content = fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
 
-console.log(lines);
+console.log(content);
